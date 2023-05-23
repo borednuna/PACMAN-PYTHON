736db21b2c67fdaa7cda28989488ad4a3fa300f0
daf2a <muhammaddaffaashdaqfillah@gmail.com>
2023-05-23 11:27:03 +0700

    add : astar method
    Co-authored-by: Hanun Shaka <borednuna@users.noreply.github.com>

--- classes/inky.py
+++ classes/inky.py
@@ -1,6 +1,9 @@
 import pygame
 import math
+from queue import PriorityQueue
 from classes.ghost import Ghost
+from classes.utility import Utility
+
 
 # Inherit Inky from Ghost
 class Inky(Ghost):
@@ -90,61 +93,144 @@ class Inky(Ghost):
                             or (level[self.center_y // num1][(self.center_x + num3) // num2] == 9 and (
                             self.is_in_box or self.is_dead)):
                         self.turns[0] = True
-        else:
+            # current_x = (self.center_x) // num2
+            # current_y = (self.center_y) // num1
+
+            # if level[(self.center_y - num3) // num1][current_x] < 3 or level[(self.center_y - num3) // num1][current_x] == 9:
+            #     self.turns[2] = True
+            # if level[(self.center_y + num3) // num1][current_x] < 3 or level[(self.center_y + num3) // num1][current_x] == 9:
+            #     self.turns[3] = True
+            #     if(level[(self.center_y + num3) // num1][current_x] == 9):
+            #         self.turns[3] = False
+            # if level[current_y][(self.center_x - num3) // num1] < 3 or level[current_y][(self.center_x - num3) // num1] == 9:
+            #     self.turns[1] = True
+            # if level[current_y][(self.center_x + num3) // num1] < 3 or level[current_y][(self.center_x + num3) // num1] == 9:
+            #     self.turns[0] = True
+
+            # print(current_x, current_y, self.turns, self.is_in_box, self.is_dead)
+
+        else: # kayaknya buat passage:v
             self.turns[0] = True
             self.turns[1] = True
-        if 350 < self.x_pos < 550 and 370 < self.y_pos < 480:
+        if 350 < self.x_pos < 550 and 330 < self.y_pos < 480: # cek box
             self.is_in_box = True
         else:
             self.is_in_box = False
         return self.turns, self.is_in_box
 
-    # # move ghost
-    # def move(self, level, HEIGHT, WIDTH):
-    #     # r, l, u, d
-    #     # inky is going to turn left or right whenever advantageous, but only up or down on collision
-    #     self.turns, self.is_in_box = self.check_collisions(level, HEIGHT, WIDTH)
+    # move ghost GBFS
+    def move_gbfs(self, level, HEIGHT, WIDTH):
+        self.turns, self.is_in_box = self.check_collisions(level, HEIGHT, WIDTH)
+        minDistance = 1000000
+        newpos = (self.x_pos, self.y_pos)
+        fixdir = (self.x_pos, self.y_pos)
+        newcenter = (self.center_x, self.center_y)
+        fixcenter = (self.center_x, self.center_y)
+
+        for i in range(len(self.turns)):
+            if self.is_in_box:
+                self.target = (420, 328)
+            
+            if self.is_dead:
+                self.target = (14 * 30, 11 * 32)
+
+            if self.turns[i]:
+                if i == 0:
+                    newpos = (self.x_pos + self.speed, self.y_pos)
+                    newcenter = (self.center_x + self.speed, self.center_y)
+                elif i == 1:
+                    newpos = (self.x_pos - self.speed, self.y_pos)
+                    newcenter = (self.center_x - self.speed, self.center_y)
+                elif i == 2:
+                    newpos = (self.x_pos, self.y_pos - self.speed)
+                    newcenter = (self.center_x, self.center_y - self.speed)
+                elif i == 3:
+                    newpos = (self.x_pos, self.y_pos + self.speed)
+                    newcenter = (self.center_x, self.center_y + self.speed)
+                distance = (newpos[0] - self.target[0]) ** 2 + (newpos[1] - self.target[1]) ** 2
+                if distance < minDistance:
+                    minDistance = distance
+                    self.direction = i
+                    fixdir = newpos
+                    fixcenter = newcenter
+
+        self.x_pos = fixdir[0]
+        self.y_pos = fixdir[1]
+
+        self.center_x = fixcenter[0]
+        self.center_y = fixcenter[1]
+
+        if self.x_pos < -30:
+            self.x_pos = 900
+            self.center_x = 922
+        elif self.x_pos > 900:
+            self.x_pos -= 30
+
+        print(self.target, self.x_pos, self.y_pos, self.turns)
+        return self.x_pos, self.y_pos, self.direction
+
+    # move ghost A*
+    def move_astar(self, level, HEIGHT, WIDTH):
+        frontier = PriorityQueue()
+        frontier.put((0, (self.x_pos, self.y_pos)))
+        came_from = {}
+        cost_so_far = {}
+        came_from[(self.x_pos, self.y_pos)] = None
+        cost_so_far[(self.x_pos, self.y_pos)] = 0
+
+        while not frontier.empty():
+            current = frontier.get()[1]
+            if current == self.target:
+                break
+            
+            self.turns, self.is_in_box = self.check_collisions(level, HEIGHT, WIDTH)
+            for i in range(len(self.turns)):
+                if self.turns[i]:
+                    if i == 0:
+                        newpos = (current[0] + self.speed, current[1])
+                    elif i == 1:
+                        newpos = (current[0] - self.speed, current[1])
+                    elif i == 2:
+                        newpos = (current[0], current[1] - self.speed)
+                    elif i == 3:
+                        newpos = (current[0], current[1] + self.speed)
+                    new_cost = cost_so_far[current] + 1
+                    if newpos not in cost_so_far or new_cost < cost_so_far[newpos]:
+                        cost_so_far[newpos] = new_cost
+                        priority = new_cost + self.heuristic(self.target, newpos)
+                        frontier.put((priority, newpos))
+                        came_from[newpos] = current
         
-    #     # using phythagorean theorem to find distance between pacman and ghost
-    #     minDistance = 1000000
-    #     newpos = (0, 0)
-    #     fixdir = (0, 0)
-    #     newcenter = (0, 0)
-    #     fixcenter = (0, 0)
-    #     for i in range(len(self.turns)):
-    #         if self.turns[i]:
-    #             if i == 0:
-    #                 newpos = (self.x_pos + self.speed, self.y_pos)
-    #                 newcenter = (self.center_x + self.speed, self.center_y)
-    #             elif i == 1:
-    #                 newpos = (self.x_pos - self.speed, self.y_pos)
-    #                 newcenter = (self.center_x - self.speed, self.center_y)
-    #             elif i == 2:
-    #                 newpos = (self.x_pos, self.y_pos - self.speed)
-    #                 newcenter = (self.center_x, self.center_y - self.speed)
-    #             elif i == 3:
-    #                 newpos = (self.x_pos, self.y_pos + self.speed)
-    #                 newcenter = (self.center_x, self.center_y + self.speed)
-    #             distance = math.sqrt((newpos[0] - self.target[0]) ** 2 + (newpos[1] - self.target[1]) ** 2)
-    #             if distance < minDistance:
-    #                 minDistance = distance
-    #                 self.direction = i
-    #                 fixdir = newpos
-    #                 fixcenter = newcenter
+        path =  self.reconstruct_path(came_from, self.target, (self.x_pos, self.y_pos))
 
-    #     self.x_pos = fixdir[0]
-    #     self.y_pos = fixdir[1]
+        if len(path) > 1:
+            if path[1][0] > self.x_pos:
+                self.direction = 0
+            elif path[1][0] < self.x_pos:
+                self.direction = 1
+            elif path[1][1] < self.y_pos:
+                self.direction = 2
+            elif path[1][1] > self.y_pos:
+                self.direction = 3
 
-    #     self.center_x = fixcenter[0]
-    #     self.center_y = fixcenter[1]
+        self.x_pos = path[1][0]
+        self.y_pos = path[1][1]
+        
+        if self.x_pos < -30:
+            self.x_pos = 900
+        elif self.x_pos > 900:
+            self.x_pos -= 30
 
-    #     if self.x_pos < -30:
-    #         self.x_pos = 900
-    #         self.center_x = 922
-    #     elif self.x_pos > 900:
-    #         self.x_pos -= 30
+        return self.x_pos, self.y_pos, self.direction
 
-    #     return self.x_pos, self.y_pos, self.direction
+    def reconstruct_path(self, came_from, goal, start):
+        current = goal
+        path = []
+        while current != start:
+            path.append(current)
+            current = came_from[current]
+        path.reverse()
+        return path
 
     # move ghost
     def move(self, level, HEIGHT, WIDTH):
--- classes/movement.py
+++
@@ -1,6 +0,0 @@
-import pygame
-import math
-from board import boards
-
-class Movement :
-    
